1bd94077bd6f663662a87f842af2510df2d973ae
crisdav98 <47307974+crisdav98@users.noreply.github.com>
2020-01-27 21:36:27 -0500

    Add files via upload

---
+++ build/precache-manifest.5716e550e6ff96ecf027f90926eb27b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4b8a9688490029f80b14eacaed165f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ad40461d8368703d9e1",
+    "url": "/static/css/main.0757cd2b.chunk.css"
+  },
+  {
+    "revision": "18577844c8c726b98e10",
+    "url": "/static/js/2.575fc223.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.575fc223.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ad40461d8368703d9e1",
+    "url": "/static/js/main.bcf7ce0f.chunk.js"
+  },
+  {
+    "revision": "3fc4155feb15f57bbb2c",
+    "url": "/static/js/runtime-main.ea334e17.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
